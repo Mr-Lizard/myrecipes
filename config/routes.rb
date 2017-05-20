2ead9e3fed41c52504446236ad94d1306479a009
K. L. Estes <klestes@gmail.com>
2017-05-20 06:27:54 +0000

    84 finished>

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   root "pages#home"
   get 'pages/home', to: 'pages#home'
   
-  #resources :recipes
-  get '/recipes', to: 'recipes#index'
-  get '/recipes/new', to: 'recipes#new',  as: 'new_recipe'
-  get '/recipes/:id', to: 'recipes#show', as: 'recipe'
-  
+  resources :recipes
+
 end
